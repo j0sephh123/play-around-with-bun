79e3569afc2c6e8f9b1c54b0f0d917d9f472723a
Joseph <iossif123gg@gmail.com>
2023-10-17 23:15:37 +0300

    add table boilerplate

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,11 +9,35 @@ function App() {
   }
 
   return (
-    <>
-      {users.map((user) => (
-        <div key={user.id}>{user.name}</div>
-      ))}
-    </>
+    <div style={{ background: "#333", color: "#FFF", padding: "20px" }}>
+      <table
+        style={{
+          width: "100%",
+          borderCollapse: "collapse",
+          borderColor: "#666",
+          color: "#FFF",
+        }}
+      >
+        <thead>
+          <tr>
+            <th style={{ border: "1px solid #666", padding: "10px" }}>ID</th>
+            <th style={{ border: "1px solid #666", padding: "10px" }}>Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(({ id, name }) => (
+            <tr key={id}>
+              <td style={{ border: "1px solid #666", padding: "10px" }}>
+                {id}
+              </td>
+              <td style={{ border: "1px solid #666", padding: "10px" }}>
+                {name}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
